d101e7f27745542660a9f1ca4c34cb514d6bc71e
lBlanc99 <ricky.anggarda@hotmail.com>
2021-12-09 06:53:49 +0700

    UI Changes according to app theme

--- app/src/main/java/com/dicoding/schoolreview/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/schoolreview/main/MainActivity.kt
@@ -2,15 +2,26 @@ package com.dicoding.schoolreview.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.schoolreview.R
+import android.view.Menu
+import android.view.MenuInflater
+
 import com.dicoding.schoolreview.databinding.ActivityMainBinding
+import com.dicoding.schoolreview.fragment.SearchFragment
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private  val searchFragment =SearchFragment()
+
+
+    lateinit var activityHomeBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val activityHomeBinding = ActivityMainBinding.inflate(layoutInflater)
+        activityHomeBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityHomeBinding.root)
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
@@ -19,10 +30,24 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.elevation = 0f
 
 
+
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       val inflater:MenuInflater =menuInflater
+        inflater.inflate(com.dicoding.schoolreview.R.menu.menu,menu)
+
+
+        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
+
+
 }
